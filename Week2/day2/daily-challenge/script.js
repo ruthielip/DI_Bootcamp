62847b465c0c960946cc3b332a6fad974c56c923
ruthielip <ruthieism@gmail.com>
2021-10-12 17:56:59 +0300

    adding daily challenge

--- Week2/day2/daily-challenge/script.js
+++ Week2/day2/daily-challenge/script.js
@@ -1,17 +1,17 @@
-let sentence = "It is not so bad to take a break";
-let wordNot = sentence.substring(6, 9);
-let wordBad = sentence.substring(13, 16);
-let notBad = sentence.substring(6, 16);
-let newWord = sentence.replace(notBad, "good")
+// let sentence = "It is not so bad to take a break";
+// let wordNot = sentence.substring(6, 9);
+// let wordBad = sentence.substring(13, 16);
+// let notBad = sentence.substring(6, 16);
+// let newWord = sentence.replace(notBad, "good")
 
-if (wordNot > wordBad) {
+// if (wordNot > wordBad) {
 
-	console.log(newWord)
+// 	console.log(newWord)
 
-} else {
+// } else {
 
-	console.log(sentence)
-}
+// 	console.log(sentence)
+// }
 
 
 // Another attempt:
@@ -32,3 +32,16 @@ if (wordNot > wordBad) {
 // } else {
 // 	console.log(sentence)
 // }
+let sentence = "It is not so bad to take a break";
+
+let arraySentence = sentence.split(" ");
+let wordNot = arraySentence.indexOf("not");
+let wordBad = arraySentence.indexOf("bad");
+
+if(wordBad>wordNot && wordNot!==-1){ 
+      arraySentence.splice(wordNot,(wordBad - wordNot +1),"good");
+      arraySentence = arraySentence.join(" ") 
+      console.log(arraySentence);
+  } else if (wordBad<wordNot || wordNot===-1 || wordBad===-1) { 
+     console.log(sentence);
+ }
---
+++ Week2/day3/daily-challenge/script.js
@@ -0,0 +1,18 @@
+let str ="";
+
+for (let i = 0; i <= 5; i++) {
+
+	star = star + "*"
+	console.log(star)
+
+}
+
+
+let star="";
+for (let i=0; i<6; i++){
+	for(let j=0; j<i+1; j++){
+		star = star + "*"
+	}
+	console.log(star)
+	star="";
+}
