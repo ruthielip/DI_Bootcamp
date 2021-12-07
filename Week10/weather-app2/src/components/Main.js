22d859b9292afb494ccb7e3d75cf981afe3c0628
ruthielip <ruthieism@gmail.com>
2021-12-07 17:17:41 +0200

    adding css

--- Week10/weather-app2/src/components/Main.js
+++ Week10/weather-app2/src/components/Main.js
@@ -1,14 +1,13 @@
 import {useState} from 'react';
 import WeatherDisplay from './WeatherDisplay';
 
-
 const Main = () => {
   const [cityText, setCityText] = useState('');
   const [countryText, setCountryText] = useState('');
   const [weather, setWeather] = useState('')
 
   const getWeather = () => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityText},${countryText}&appid=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityText},${countryText}&appid=c26c2c63db865467c1cab559f63e31da`)
     .then(res=>res.json())
     .then(data=>{
       setWeather(data)
@@ -18,12 +17,19 @@ const Main = () => {
   console.log(weather)
   return(
     <div className='main'>
-      <h1>Weather Finder</h1>
-      <p className='desc'>Find out temperature, conditions and more...</p>
+
       <div className='display'>
-         <input type='text' placeholder='City...' onChange={(e)=>setCityText(e.target.value)}/>
-         <input type='text' placeholder='Country...' onChange={(e)=>setCountryText(e.target.value)}/>
-         <button onClick={getWeather}>Get Weather</button>
+      <div className='title'>
+        <div className='header'>
+          <h1>Weather Finder</h1>
+          <i class="fas fa-meteor"></i>
+        </div>
+        <div className='form'>
+           <input type='text' placeholder='City...' onChange={(e)=>setCityText(e.target.value)}/><br/>
+           <input type='text' placeholder='Country...' onChange={(e)=>setCountryText(e.target.value)}/><br/>
+           <button onClick={getWeather}>Get Weather</button>
+        </div>
+      </div>
          <WeatherDisplay display={weather}></WeatherDisplay>
       </div>
     </div>
--- Week10/weather-app2/src/components/WeatherDisplay.js
+++ Week10/weather-app2/src/components/WeatherDisplay.js
@@ -11,10 +11,10 @@ const WeatherDisplay = (props) => {
   } else {
     return(
       <div className='weather'>
-         <p><span className='label'>Loaction:</span> {display.name}, {display.sys.country}</p>
-         <p><span className='label'>Temperature:</span> {display.main.temp}</p>
-         <p><span className='label'>Humidity:</span> {display.main.humidity}</p>
-         <p><span className='label'>Condition:</span> {display.weather[0].description}</p>
+         <p><span className='label'>Loaction:</span><br/> {display.name}, {display.sys.country}</p>
+         <p><span className='label'>Temperature:</span><br/> {display.main.temp}</p>
+         <p><span className='label'>Humidity:</span><br/> {display.main.humidity}</p>
+         <p><span className='label'>Condition:</span><br/> {display.weather[0].description}</p>
       </div>
     )
   }
